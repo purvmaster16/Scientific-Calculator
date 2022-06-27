61c32de79d598afbf5d28207395b9e26947a37f3
purvmaster16 <73308820+purvmaster16@users.noreply.github.com>
2022-06-27 21:05:24 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head >
+<style>
+h1{
+			color: cyan;
+			background-color: orange;
+			padding:10px; 
+			border-radius: 50px;
+			
+		}
+</style>
+	<center>
+	<h1> WELLCOME </h1>
+		<h1> Pls enter your details </h1>
+	</center>
+</head>
+<body bgcolor="grey">
+	<center>
+	<form method="post" action="Scientific Calculator.php">
+	    User Name:<input type="text" name="un">
+		password:<input type="password" name="pwd">
+		<input type="submit" value="submit">
+	</form>
+	</center>
+
+</body>
+</html>
